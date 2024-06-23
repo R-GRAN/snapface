a836e5d6262fc27fdc4842b021a00ff0c1db729b
Remy <remy.grangenois@gmail.com>
2024-06-23 18:15:36 +0200

    Add Snapped class when snapped, Date format with local setting and upperCase title format

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,NgClass,UpperCasePipe,DatePipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
