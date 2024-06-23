5f83203ac01917ca22d79eed818b4aa8b039b29c
R-GRAN <144110636+R-GRAN@users.noreply.github.com>
2024-06-23 20:02:51 +0200

    Revert "Feat/header app"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import { FaceSnap } from './models/face-snap';
-import { HeaderComponent } from './header/header.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,FaceSnapComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
-
-export class AppComponent implements OnInit {
-  newFaceSnap!: FaceSnap;
-
-  ngOnInit(): void {
-    this.newFaceSnap = new FaceSnap(
-      'Joli Teddy',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      "Mon meilleur ami d'enfance",
-      new Date(),
-      148
-    );
-  }
+export class AppComponent {
+  title = 'snapface';
 }
