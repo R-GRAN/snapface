ee749d403c70664e321310e1b5b45b085a66af24
Remy <remy.grangenois@gmail.com>
2024-06-25 21:24:05 +0200

    form userEmail for newsletter

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  userEmail!: string;
+
   constructor(private router: Router) {}
-  onContinue():void {
+  onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
   }
 }
